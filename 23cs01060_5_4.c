3cb38e6b7f2de28575ed0361c749058d3e2c5d84
skshmgrg <152769569+skshmgrg@users.noreply.github.com>
2024-02-15 22:27:03 +0530

    Update 23cs01060_5_4.c

--- 23cs01060_5_4.c
+++ 23cs01060_5_4.c
@@ -4,9 +4,9 @@ int main()
     int i,j,n,k;
      printf("enter the number of rows of the pyramid\t");
      scanf("%d",&n);
-    for(i=1;i<=4;i++)
+    for(i=1;i<=n;i++)
     {
-        for(j=3; j>=i; j--) printf("'");
+        for(j=1; j<n-(i-1); j++) printf(" ");
         for(k=1;k<=i;k++) printf("* ");
         printf("\n");
     }
